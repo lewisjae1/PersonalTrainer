9744e311749b9820a8b8fbfd73b231f708e95927
Jaein Lewis <dlwodls0115@gmail.com>
2022-04-11 20:19:06 -0700

    Add project files.

---
+++ PersonalTrainer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PersonalTrainer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
