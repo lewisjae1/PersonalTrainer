5013a9ae707dde7979c1b0372b61f623e9aa1d5e
Jaein Lewis <dlwodls0115@gmail.com>
2022-04-30 22:01:09 -0700

    Added gender to the identity database

---
+++ PersonalTrainer/Models/MyCustomUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalTrainer.Models
+{
+    public class MyCustomUser : IdentityUser
+    {
+        [Required]
+        public string Gender { get; set; }
+    }
+}
