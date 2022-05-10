8f08e80f97e56fbd61e294938b7f0ea8a240c02b
Jaein Lewis <dlwodls0115@gmail.com>
2022-05-09 23:52:28 -0700

    Added first name, last name, and date of birth

--- PersonalTrainer/Models/MyCustomUser.cs
+++ PersonalTrainer/Models/MyCustomUser.cs
@@ -7,5 +7,14 @@ namespace PersonalTrainer.Models
     {
         [Required]
         public string Gender { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime DateofBirth { get; set; }
     }
 }
