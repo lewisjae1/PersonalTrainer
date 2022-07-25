6ebf936deb150b59f4a5ecde7999678a2a474bf6
Jaein Lewis <dlwodls0115@gmail.com>
2022-07-24 19:21:58 -0700

    Set up an class for trainers

---
+++ PersonalTrainer/Models/Trainer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersonalTrainer.Models
+{
+    public class Trainer
+    {
+        public int TrainerId { get; set; }
+
+        [ForeignKey("MyCustomUser")]
+        public string userId { get; set; }
+        public MyCustomUser MyCustomUser { get; set; }
+
+        public bool Listed { get; set; }
+    }
+}
