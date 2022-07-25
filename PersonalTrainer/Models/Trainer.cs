26200c1cc3b05b47023df33c7436b85be909968e
Jaein Lewis <dlwodls0115@gmail.com>
2022-07-24 20:48:43 -0700

    Edited userId to Id, so the foreign key work with EF core

--- PersonalTrainer/Models/Trainer.cs
+++ PersonalTrainer/Models/Trainer.cs
@@ -1,13 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PersonalTrainer.Models
 {
     public class Trainer
     {
+        [Key]
         public int TrainerId { get; set; }
 
         [ForeignKey("MyCustomUser")]
-        public string userId { get; set; }
+        public string Id { get; set; }
         public MyCustomUser MyCustomUser { get; set; }
 
         public bool Listed { get; set; }
