ec85ca94d2f4847377a1975926ae51b49aa13e21
Jaein Lewis <dlwodls0115@gmail.com>
2022-07-24 22:41:12 -0700

    Created views and controller for trainer list

---
+++ PersonalTrainer/Controllers/TrainersListController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PersonalTrainer.Data;
+using PersonalTrainer.Models;
+
+namespace PersonalTrainer.Controllers
+{
+    public class TrainersListController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TrainersListController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TrainersList
+        public async Task<IActionResult> Index(int? id)
+        {
+            const int NumGamesToDisplayPerPage = 3;
+            const int PageOffset = 1; // Need a page offset to use current page and figure out, num games to skip
+            int currentPage = id ?? 1;
+
+            int totalNumOfProducts = await _context.Trainers.CountAsync();
+            double maxNumPages = Math.Ceiling((double)totalNumOfProducts / NumGamesToDisplayPerPage);
+            int lastPage = Convert.ToInt32(maxNumPages); // Rounding pages up, to next whole page number
+
+            // Get all games from the DB
+            List<Trainer> trainers = await _context.Trainers
+                                    .Skip(NumGamesToDisplayPerPage * (currentPage - PageOffset))
+                                    .Take(NumGamesToDisplayPerPage).Where(t => t.Listed == true).ToListAsync();
+
+            TrainerListViewModel TrainerListModel = new(trainers, lastPage, currentPage);
+            return View(TrainerListModel);
+        }
+
+        // GET: TrainersList/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Trainers == null)
+            {
+                return NotFound();
+            }
+
+            var trainer = await _context.Trainers
+                .Include(t => t.MyCustomUser)
+                .FirstOrDefaultAsync(m => m.TrainerId == id);
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+
+            return View(trainer);
+        }
+
+        // GET: TrainersList/Create
+        public IActionResult Create()
+        {
+            ViewData["Id"] = new SelectList(_context.MyCustomUsers, "Id", "Id");
+            return View();
+        }
+
+        // POST: TrainersList/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TrainerId,Id,Listed")] Trainer trainer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(trainer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Id"] = new SelectList(_context.MyCustomUsers, "Id", "Id", trainer.Id);
+            return View(trainer);
+        }
+
+        // GET: TrainersList/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Trainers == null)
+            {
+                return NotFound();
+            }
+
+            var trainer = await _context.Trainers.FindAsync(id);
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+            ViewData["Id"] = new SelectList(_context.MyCustomUsers, "Id", "Id", trainer.Id);
+            return View(trainer);
+        }
+
+        // POST: TrainersList/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TrainerId,Id,Listed")] Trainer trainer)
+        {
+            if (id != trainer.TrainerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(trainer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TrainerExists(trainer.TrainerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Id"] = new SelectList(_context.MyCustomUsers, "Id", "Id", trainer.Id);
+            return View(trainer);
+        }
+
+        // GET: TrainersList/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Trainers == null)
+            {
+                return NotFound();
+            }
+
+            var trainer = await _context.Trainers
+                .Include(t => t.MyCustomUser)
+                .FirstOrDefaultAsync(m => m.TrainerId == id);
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+
+            return View(trainer);
+        }
+
+        // POST: TrainersList/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Trainers == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Trainers'  is null.");
+            }
+            var trainer = await _context.Trainers.FindAsync(id);
+            if (trainer != null)
+            {
+                _context.Trainers.Remove(trainer);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TrainerExists(int id)
+        {
+          return (_context.Trainers?.Any(e => e.TrainerId == id)).GetValueOrDefault();
+        }
+    }
+}
