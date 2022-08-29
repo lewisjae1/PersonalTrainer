c65fdaee9a916808e5fd5802ca4acd32003474b9
Jaein Lewis <dlwodls0115@gmail.com>
2022-08-29 04:49:19 -0700

    Made view model to display list of trainers that client has inquired to

---
+++ PersonalTrainer/Models/ClientInquiryListViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace PersonalTrainer.Models
+{
+    public class ClientInquiryListViewModel
+    {
+        public ClientInquiryListViewModel(List<Trainer> trainers, List<MyCustomUser> allUsers, List<Inquiry> inquiries,
+            int lastPage, int currentPage)
+        {
+            Trainers = trainers;
+            AllUsers = allUsers;
+            Inquiries = inquiries;
+            LastPage=lastPage;
+            CurrentPage=currentPage;
+        }
+
+        public List<Trainer> Trainers { get; set; }
+
+        public List<MyCustomUser> AllUsers { get; set; }
+
+        public List<Inquiry> Inquiries { get; set; }
+
+        public int LastPage { get; set; }
+
+        public int CurrentPage { get; set; }
+    }
+}
