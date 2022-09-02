ce539c6767d427d6bf2e13c6ea1ed0a53279206b
Jaein Lewis <dlwodls0115@gmail.com>
2022-09-02 01:24:34 -0700

    Implemented web API from wger

---
+++ PersonalTrainer/Data/API/ExerciseCategories.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace PersonalTrainer.Data.API
+{
+    public class ExerciseCategories
+    {
+        [JsonProperty("count")]
+        public int Count { get; set; }
+
+        [JsonProperty("next")]
+        public string Next { get; set; }
+
+        [JsonProperty("previous")]
+        public string Previous { get; set; }
+
+        [JsonProperty("results")]
+        public Category[] Results { get; set; }
+    }
+
+    public class Category
+    {
+        public int id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
